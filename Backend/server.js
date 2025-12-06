b3fb66651ea8244f7c21f5680eb97e23b63f439f
Rudy Mar <140030982+Barusu0@users.noreply.github.com>
2025-12-06 11:00:52 +0800

    Fix nodemailer SMTP + backend port

--- Backend/server.js
+++ Backend/server.js
@@ -155,7 +155,8 @@ pool.query("SELECT NOW()", (err) => {
 
 // Nodemailer setup
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
